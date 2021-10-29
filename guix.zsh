66a7eaeb1ccaf1bf537bf6421586fbaa43623b3f
William <wjcaldwell@besport.com>
2021-10-29 18:05:25 +0200

    force /home/wjc/.guix-extra-profiles

--- guix.zsh
+++ guix.zsh
@@ -1,4 +1,7 @@
 unset GUIX_PROFILE
+if [ -z $GUIX_EXTRA_PROFILES ]; then
+  GUIX_EXTRA_PROFILES=/home/wjc/.guix-extra-profiles
+fi
 for p in ~/conf/guix/profiles/*; do
         pn=$(basename $p)
         if [ -e $GUIX_EXTRA_PROFILES/$pn/etc/profile ]; then
